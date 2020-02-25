b991d32259cc2c8c5519e3ab865ea034b3638bc2
Juan <juan.elca@gmail.com>
2020-02-25 16:00:09 -0300

    update cripts

---
+++ API/init_app.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip3 install flask
+sudo pip3 install SQLAlchemy
+sudo pip3 install datetime
+sudo pip3 install requests
+python3 init_app.py
